873118fbeb4be74747a1d9b1cdd8e14075dfd7c8
Mohammad Ali Ibrahim <mohamibr@c2r6s2.42beirut.com>
2025-02-20 16:57:40 +0200

    starting_ex00

---
+++ starting/ex00/Hello.py
@@ -0,0 +1,9 @@
+ft_list = ["Hello", "World!"]
+ft_tuple = ("Hello", "Lebanon!")
+ft_set = {"Beirut!", "Hello"}
+ft_dict = {"Hello" : "42Beirut!"}
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
